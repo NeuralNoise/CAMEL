a9f7097c05b4fe2738d27b042c22d45c42618a39
Joe Christiansen <christiansenjoe123@outlook.com>
2016-07-11 17:02:57 +0000

    did a lot more stuff

---
+++ app.js
@@ -0,0 +1,12 @@
+/*var start = document.getElementsByClassName('startMenu')
+
+document.getElementsByClassName('userImg').onclick = function() {
+    start.classList.add('startAnima');
+  } else {
+    this.innerHTML = 'Play';
+    var computedStyle = window.getComputedStyle(start),
+        marginLeft = computedStyle.getPropertyValue('margin-left');
+    start.style.marginLeft = marginLeft;
+    start.classList.remove('horizTranslate');    
+  }  
+}*/
